21fb4b876561433cfe35d87aabd836f4db8b2461
Luca <lopritescu@institutmvm.cat>
2026-01-30 10:23:36 +0100

    Add files via upload

---
+++ Vehicle.py
@@ -0,0 +1,17 @@
+class Vehicle(self):
+
+    def __init__(self, matricula, model, kms_inicials):
+        self.matricula = matricula
+        self._model = model 
+        self.__kms = kms_inicials 
+
+    def llegir_kms(self):
+        return self.__kms
+
+    def actualitzar_kms(self, nous_kms):
+        if nous_kms < self.__kms:
+            print("Error: Algú intenta trucar el comptador!")
+            return False 
+        else:
+            self.__kms = nous_kms
+            return True
